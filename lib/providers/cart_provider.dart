71f7eef922b11a67fd206749a0a26f87d9b4b1fb
vitaliivovchok <71486507+vitalii-vov@users.noreply.github.com>
2025-02-03 11:11:57 -0600

    Notifier

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/models/product.dart';
+
+class CartNotifier extends Notifier<Set<Product>> {
+  @override
+  Set<Product> build() {
+    return {
+      const Product(id: '4', title: 'Red Backpack', price: 14, image: 'assets/products/backpack.png'),
+    };
+  }
+
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+}
+
+final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+  return CartNotifier();
+});
